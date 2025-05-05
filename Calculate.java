a46928799a989959473be48d022d226ebda31fe0
SahooSriya <sriyasahoo3888@gmail.com>
2025-05-05 23:58:36 +0530

    add: creating css

--- Calculate.java
+++ Calculate.java
@@ -1,55 +1,63 @@
 import java.util.Scanner;
-public class Calculate {
+
+public class Calculate{
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of Subjects: ");
-        int sheet = sc.nextInt();
-        sc.nextLine();
+        Scanner scanner = new Scanner(System.in);
 
-        String[] subjnames = new String[sheet];
-        int[] marks = new int[sheet];
-        int totalmarks = 0;
-         for(int i = 0; i<sheet; i++){
-            System.out.println("Enter your subjects name: ");
-            subjnames[i] = sc.nextLine();
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        scanner.nextLine();
 
-            while(true) {
-                System.out.println("Enter marks for" + subjnames[i] + "(out of 100) " );
-                marks[i] = sc.nextInt();
-                sc.nextLine();
+        String[] subjects = new String[numSubjects];
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
 
-                if (marks[i] >= 0 && marks[i] <= 100){
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter the name of subject " + (i + 1) + ": ");
+            subjects[i] = scanner.nextLine();
+
+            while (true) {
+                System.out.print("Enter marks for " + subjects[i] + " (out of 100): ");
+                marks[i] = scanner.nextInt();
+                scanner.nextLine();
+
+                if (marks[i] >= 0 && marks[i] <= 100) {
                     break;
                 } else {
-                    System.out.println("Invalid marks! please enter your marks between 1 to 100 ");
+                    System.out.println("Invalid input! Please enter marks between 0 and 100.");
                 }
             }
-             totalmarks += marks[i];
-         }
-                double avgper = (double) totalmarks / sheet;
 
-                char grade;
+            totalMarks += marks[i];
+        }
 
-                if(avgper >= 90){
-                    grade = 'A';
-                } else if (avgper >= 80){
-                    grade = 'c';
-                }else if (avgper >= 60){
-                    grade = 'D';
-                }else if (avgper >= 50){
-                    grade = 'E';
-                }else{
-                    grade = 'F';
-                }
-                System.out.println("Subject wise marks: ");
-                for (int i = 0; i < sheet; i++) {                    
-                    System.out.println(subjnames[i] + " : " + marks[i]);               
-                }
-                System.out.println("total marks: "+ totalmarks);
-                System.out.println("Average marks: "+ avgper + "%");
-                System.out.println("Grade: "+ grade);
-                sc.close();
-         
-             }
-     }
+        double averagePercentage = (double) totalMarks / numSubjects;
+        char grade;
+
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else if (averagePercentage >= 50) {
+            grade = 'E';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\nSubject-wise Marks:");
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.println(subjects[i] + ": " + marks[i]);
+        }
+
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
